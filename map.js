62a306141e4c8fb2fda87e49e781c89e6d9d6d6e
dpoli2 <78754561+dpoli2@users.noreply.github.com>
2021-04-24 19:11:32 -0400

    Update map.js

--- map.js
+++ map.js
@@ -9,13 +9,33 @@ var circles = d3.selectAll("circle");
 // Resets color scale using month and chemical values
 function setScale()
 {
-
+   color_scale = d3.scale.linear()
+            .domain(data[chemical][1], data[chemical][2])
+            .range("red", "blue");
 }
 
 // Updates colors of circles, using color_scale, and dataset for chemical and month.
 function updateColor() 
 {
-
+  circles.data(data) 
+            .enter()
+            .append("circle")
+            .attr("fill", function(d)
+                {
+                    if(d[chemical][3][month][2][0] == -1){
+                        return "black"
+                    }
+                    else{
+                        return color_scale(d[chemical][3][month][2][0])
+                    }
+                })
 }
 
 console.log(data[0]);
+console.log(data[0][2]);
+console.log(data[0]//chemical
+                [3]//3rd element, array of first year
+                [0]//month
+                [2]//data
+                [0]//single data point
+                )
