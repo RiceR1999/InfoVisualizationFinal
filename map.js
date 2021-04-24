7611da2ae26646e76530a55ba0278f4f109129b2
Gavin Coulthurst <coulth15@students.rowan.edu>
2021-04-24 17:54:41 -0400

    Added skeleton for d3 code

---
+++ map.js
@@ -0,0 +1,21 @@
+// Keeps track of where in the data we are
+var chemical = 0;
+var month = 0;
+
+// D3 color scale to set color of symbols
+var color_scale;
+var circles = d3.selectAll("circle");
+
+// Resets color scale using month and chemical values
+function setScale()
+{
+
+}
+
+// Updates colors of circles, using color_scale, and dataset for chemical and month.
+function updateColor() 
+{
+
+}
+
+console.log(data[0]);
